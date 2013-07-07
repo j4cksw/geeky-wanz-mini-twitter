a46aa17b3398c954cc0c4fd34b124e2ca01dc8be
Natthapong <sendmanz@gmail.com>
2013-07-07 16:35:16 +0700

    fix 0 - 20 post

--- src/main/java/com/geeky/springmvc/repository/PostDataRepository.java
+++ src/main/java/com/geeky/springmvc/repository/PostDataRepository.java
@@ -34,7 +34,7 @@ public class PostDataRepository {
 	
 	public List<PostData> getAll(String userID) {
 		List<PostData> listData = new ArrayList<PostData>();
-		for (Object postData: redisTemplate.opsForList().range(userID, 0, 20)) {
+		for (Object postData: redisTemplate.opsForList().range(userID, 0, 19)) {
 			Gson gson = new GsonBuilder().setDateFormat(DateFormat.FULL, DateFormat.FULL).create();
 			PostData out = gson.fromJson((String)postData, PostData.class);
 			listData.add(out);
